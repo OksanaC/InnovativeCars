ee375cd5bf8b2014f5170bc73002447e05a0aec1
OksanaC <97809697+OksanaC@users.noreply.github.com>
2022-09-26 18:40:08 +0200

    1 component (task9)

--- force-app/main/default/lwc/vehicleList/vehicleList.js
+++ force-app/main/default/lwc/vehicleList/vehicleList.js
@@ -7,10 +7,11 @@ import {
     getRelatedListRecordsBatch
 } from 'lightning/uiRelatedListApi';
 
+
 import getAccountList from '@salesforce/apex/VehicleListController.getAccount';
 import getShowroom from '@salesforce/apex/VehicleListController.getShowroom';
 import getVehicle from '@salesforce/apex/VehicleListController.getVehicle';
-import { NavigationMixin } from 'lightning/navigation';
+
 
 const columns = [{
     label: 'Vehicle Name',
@@ -20,6 +21,8 @@ const columns = [{
 fieldName: 'Brand__c',
 }];
 
+
+
 export default class AccountList extends LightningElement {
     @track accountId = '';
     @track showroomId;
@@ -27,8 +30,7 @@ export default class AccountList extends LightningElement {
     @track vehicles;
     @track columns = columns;
     @track selectedVehicles;
-    //  invoke apex method with wire property and fetch picklist options.
-    // pass 'object information' and 'picklist field API name' method params which we need to fetch from apex
+  
     @wire(getAccountList) accounts;
     @wire(getShowroom, {
         accountId: '$accountId'
@@ -41,20 +43,17 @@ export default class AccountList extends LightningElement {
         console.log(this.showrooms)
         const selectedAccount = event.target.value;
         this.accountId = event.target.value;
-    
+     
     }
 
     onValueSelection2(event) {
         const selectedShowroom = event.target.value;
         this.showroomId = event.target.value;
-     
+   
     }
     onVehicleSelection(event) {
         console.log(event.detail.selectedRows)
         this.selectedVehicles = event.detail.selectedRows;
         this.showroomId = event.target.value;
     }
- 
-
-	}
-
+}
